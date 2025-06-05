8c11940444bef3d7a5bfcacb592a001f127a8b47
cagecorn <lmcbv@naver.com>
2025-06-05 22:45:14 +0900

    Add mana system, homing projectile, and tests

---
+++ tests/homingProjectile.test.js
@@ -0,0 +1,51 @@
+const { JSDOM } = require('jsdom');
+const path = require('path');
+
+async function run() {
+  const dom = await JSDOM.fromFile(path.join(__dirname, '..', 'index.html'), {
+    runScripts: 'dangerously',
+    resources: 'usable',
+    url: 'http://localhost'
+  });
+
+  await new Promise(resolve => {
+    if (dom.window.document.readyState === 'complete') resolve();
+    else dom.window.addEventListener('load', resolve);
+  });
+
+  dom.window.updateStats = () => {};
+  dom.window.updateMercenaryDisplay = () => {};
+  dom.window.updateInventoryDisplay = () => {};
+  dom.window.renderDungeon = () => {};
+  dom.window.updateCamera = () => {};
+  dom.window.updateSkillDisplay = () => {};
+  dom.window.requestAnimationFrame = fn => fn();
+
+  const { gameState, createMonster, createHomingProjectile, processProjectiles, getDistance } = dom.window;
+
+  const monster = createMonster('ZOMBIE', gameState.player.x + 3, gameState.player.y);
+  gameState.monsters.push(monster);
+  gameState.dungeon[monster.y][monster.x] = 'monster';
+  gameState.dungeon[monster.y][monster.x - 1] = 'empty';
+  gameState.dungeon[monster.y][monster.x - 2] = 'empty';
+
+  const proj = createHomingProjectile(gameState.player.x, gameState.player.y, monster);
+
+  let prevDist = getDistance(proj.x, proj.y, monster.x, monster.y);
+  processProjectiles();
+  let newDist = getDistance(proj.x, proj.y, monster.x, monster.y);
+  if (newDist >= prevDist) {
+    console.error('projectile did not move toward target');
+    process.exit(1);
+  }
+
+  prevDist = newDist;
+  processProjectiles();
+  newDist = getDistance(proj.x, proj.y, monster.x, monster.y);
+  if (newDist >= prevDist) {
+    console.error('projectile did not continue homing');
+    process.exit(1);
+  }
+}
+
+run().catch(e => { console.error(e); process.exit(1); });
---
+++ tests/mana.test.js
@@ -0,0 +1,55 @@
+const { JSDOM } = require('jsdom');
+const path = require('path');
+
+async function run() {
+  const dom = await JSDOM.fromFile(path.join(__dirname, '..', 'index.html'), {
+    runScripts: 'dangerously',
+    resources: 'usable',
+    url: 'http://localhost'
+  });
+
+  await new Promise(resolve => {
+    if (dom.window.document.readyState === 'complete') resolve();
+    else dom.window.addEventListener('load', resolve);
+  });
+
+  dom.window.updateStats = () => {};
+  dom.window.updateMercenaryDisplay = () => {};
+  dom.window.updateInventoryDisplay = () => {};
+  dom.window.renderDungeon = () => {};
+  dom.window.updateCamera = () => {};
+  dom.window.updateSkillDisplay = () => {};
+  dom.window.requestAnimationFrame = fn => fn();
+
+  const { gameState, assignSkill, skill1Action, movePlayer, createMonster } = dom.window;
+
+  gameState.player.skills.push('Fireball');
+  assignSkill(1, 'Fireball');
+
+  const monster = createMonster('ZOMBIE', gameState.player.x + 1, gameState.player.y);
+  gameState.monsters.push(monster);
+  gameState.dungeon[monster.y][monster.x] = 'monster';
+
+  gameState.player.mana = 3;
+  skill1Action();
+  if (gameState.player.mana !== 1) {
+    console.error('mana not used or regenerated correctly');
+    process.exit(1);
+  }
+
+  movePlayer(1, 0);
+  if (gameState.player.mana !== 2) {
+    console.error('mana did not regenerate on turn');
+    process.exit(1);
+  }
+
+  const before = gameState.projectiles.length;
+  gameState.player.mana = 0;
+  skill1Action();
+  if (gameState.projectiles.length !== before) {
+    console.error('skill should not fire without mana');
+    process.exit(1);
+  }
+}
+
+run().catch(e => { console.error(e); process.exit(1); });
